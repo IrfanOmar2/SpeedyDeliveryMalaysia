a8bbcd16265d42c71fecd80a16081f574a26fb7e
IrfanOmar2 <93089809+IrfanOmar2@users.noreply.github.com>
2022-01-23 00:50:07 +0800

    Add files via upload
    SPEEDY DELIVERY MALAYSIA
    KIM1105F
    
    1. MOHD AMIRULAMIN BIN SA'AIDIN (2019278802)
    2. WAN MOHAMAD AIMAN BIN WAN MOHAMAD NAZRI (2019283866)
    3. MUHAMAD DANISH IRFAN BIN YUSNI (2019409614)
    4. MUHAMMAD IRFAN BIN OMAR (2019210506)
    5. NURUL SAHIRA BINTI ROZI (2019436284)

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+//  course section owl carousel
+
+$(".course_owl-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 3,
+    autoHeight: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    autoWidth: true
+});
+
